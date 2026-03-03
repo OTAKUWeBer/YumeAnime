4b125527ff7262dea06772b98bbe6c49229dce8a
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2026-03-03 15:46:12 +0600

    Slide animation in Spotlight

--- api/static/js/carousel.js
+++ api/static/js/carousel.js
@@ -4,20 +4,35 @@ class SpotlightCarousel {
     this.currentSlide = 0
     this.totalSlides = totalSlides
     this.autoSlideInterval = null
+    this.touchStartX = 0
+    this.touchEndX = 0
+    this.isAnimating = false
     this.init()
   }
 
   init() {
     this.startAutoSlide()
     this.setupEventListeners()
+    this.connectNavigationButtons()
   }
 
-  showSlide(index) {
+  showSlide(index, direction = "next") {
+    if (this.isAnimating || this.totalSlides === 0) return
+    
+    this.isAnimating = true
     const slides = document.querySelectorAll(".spotlight-slide")
     const indicators = document.querySelectorAll(".indicator")
 
     slides.forEach((slide, i) => {
-      slide.classList.toggle("active", i === index)
+      if (i === index) {
+        slide.classList.remove("slide-left", "slide-right")
+        slide.classList.add("active", direction === "next" ? "slide-right" : "slide-left")
+        setTimeout(() => {
+          slide.classList.remove("slide-left", "slide-right")
+        }, 600)
+      } else {
+        slide.classList.remove("active", "slide-left", "slide-right")
+      }
     })
 
     indicators.forEach((indicator, i) => {
@@ -29,21 +44,27 @@ class SpotlightCarousel {
         indicator.classList.add("bg-white/30")
       }
     })
+
+    setTimeout(() => {
+      this.isAnimating = false
+    }, 600)
   }
 
   nextSlide() {
     this.currentSlide = (this.currentSlide + 1) % this.totalSlides
-    this.showSlide(this.currentSlide)
+    this.showSlide(this.currentSlide, "next")
   }
 
   previousSlide() {
     this.currentSlide = (this.currentSlide - 1 + this.totalSlides) % this.totalSlides
-    this.showSlide(this.currentSlide)
+    this.showSlide(this.currentSlide, "prev")
   }
 
   goToSlide(index) {
+    if (index === this.currentSlide) return
+    const direction = index > this.currentSlide ? "next" : "prev"
     this.currentSlide = index
-    this.showSlide(this.currentSlide)
+    this.showSlide(this.currentSlide, direction)
   }
 
   startAutoSlide() {
@@ -54,6 +75,29 @@ class SpotlightCarousel {
     clearInterval(this.autoSlideInterval)
   }
 
+  connectNavigationButtons() {
+    const prevBtn = document.getElementById("spotlight-prev")
+    const nextBtn = document.getElementById("spotlight-next")
+
+    if (prevBtn) {
+      prevBtn.addEventListener("click", (e) => {
+        e.stopPropagation()
+        this.stopAutoSlide()
+        this.previousSlide()
+        this.startAutoSlide()
+      })
+    }
+
+    if (nextBtn) {
+      nextBtn.addEventListener("click", (e) => {
+        e.stopPropagation()
+        this.stopAutoSlide()
+        this.nextSlide()
+        this.startAutoSlide()
+      })
+    }
+  }
+
   setupEventListeners() {
     const carousel = document.getElementById("spotlight-carousel")
     if (carousel) {
@@ -61,25 +105,51 @@ class SpotlightCarousel {
       carousel.addEventListener("mouseleave", () => this.startAutoSlide())
       
       // Touch/Swipe support for mobile
-      let touchStartX = 0
-      let touchEndX = 0
-      
       carousel.addEventListener("touchstart", (e) => {
-        touchStartX = e.changedTouches[0].screenX
+        this.touchStartX = e.changedTouches[0].screenX
         this.stopAutoSlide()
       }, false)
       
       carousel.addEventListener("touchend", (e) => {
-        touchEndX = e.changedTouches[0].screenX
+        this.touchEndX = e.changedTouches[0].screenX
         this.handleSwipe()
         this.startAutoSlide()
       }, false)
+
+      // Mouse swipe support
+      let mouseStartX = 0
+      let isMouseDown = false
+
+      carousel.addEventListener("mousedown", (e) => {
+        mouseStartX = e.clientX
+        isMouseDown = true
+      })
+
+      carousel.addEventListener("mouseup", (e) => {
+        if (!isMouseDown) return
+        isMouseDown = false
+        const mouseEndX = e.clientX
+        const swipeThreshold = 50
+        const diff = mouseStartX - mouseEndX
+
+        if (Math.abs(diff) > swipeThreshold) {
+          if (diff > 0) {
+            this.nextSlide()
+          } else {
+            this.previousSlide()
+          }
+        }
+      })
+
+      carousel.addEventListener("mouseleave", () => {
+        isMouseDown = false
+      })
     }
   }
 
   handleSwipe() {
     const swipeThreshold = 50
-    const diff = touchStartX - touchEndX
+    const diff = this.touchStartX - this.touchEndX
     
     if (Math.abs(diff) > swipeThreshold) {
       if (diff > 0) {
