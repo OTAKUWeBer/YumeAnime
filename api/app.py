cf8557e02b2deb29fd6191a2d5ba6a4d19306a8f
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2026-03-16 00:59:13 +0600

    Fix server side problems

--- api/app.py
+++ api/app.py
@@ -102,5 +102,16 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import sys
+    try:
+        app.run(debug=True)
+    except KeyboardInterrupt:
+        print("Server gracefully stopped.")
+    except OSError as e:
+        if getattr(e, 'winerror', None) == 10038:
+            print("Server gracefully stopped (socket released).")
+        else:
+            raise
+    except BaseException:     
+        sys.exit(0)
--- run.py
+++ run.py
@@ -4,4 +4,15 @@ from api.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import sys
+    try:
+        app.run(debug=True)
+    except KeyboardInterrupt:
+        print("Server gracefully stopped.")
+    except OSError as e:
+        if getattr(e, 'winerror', None) == 10038:
+            print("Server gracefully stopped (socket released).")
+        else:
+            raise
+    except BaseException:     
+        sys.exit(0)
